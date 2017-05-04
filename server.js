5852e91b0d83003529c9dcc0b0fa94a6966268d0
Matt Huntington <matt.huntington@gmail.com>
2017-05-03 21:36:58 -0400

    CORS

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/stupidco
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json());
 
 var commentsController = require('./controllers/comments.js');
